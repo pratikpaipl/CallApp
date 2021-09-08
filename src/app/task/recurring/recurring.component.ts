3ea65156f44a03bbeb46de68df0c4e9566387a4c
pratikpaipl <pratikpa.aipl@gmail.com>
2021-09-08 23:56:50 +0530

    create task flow with recurring options

--- src/app/task/recurring/recurring.component.ts
+++ src/app/task/recurring/recurring.component.ts
@@ -1,5 +1,7 @@
+import { element } from 'protractor';
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { PopoverController, NavParams } from '@ionic/angular';
+import * as moment from 'moment';
 import { NavigationService } from 'src/app/services/NavigationService';
 import { GlobalProvider } from 'src/app/shared/GlobalProvider';
 import { StorageService } from 'src/app/shared/StorageService';
@@ -12,30 +14,74 @@ import { StorageService } from 'src/app/shared/StorageService';
 export class RecurringComponent implements OnInit {
 
   isRecurring: any = 'daily'
+  selectedMonth: any = '1'
+  selectedYear: any = '1'
 
   data: any;
   startDate: any;
+  recurringData: any;
   day = []
 
   constructor(public store: StorageService, private navigation: NavigationService,public popover:PopoverController,public navParams: NavParams , public globle: GlobalProvider) {
 
-    this.day.push({ day: 'Sun', isCheck: false })
-    this.day.push({ day: 'Mon', isCheck: false })
-    this.day.push({ day: 'Tue', isCheck: false })
-    this.day.push({ day: 'Wed', isCheck: false })
-    this.day.push({ day: 'Thu', isCheck: false })
-    this.day.push({ day: 'Fri', isCheck: false })
-    this.day.push({ day: 'Sat', isCheck: false })
 
+    const today = moment().format("ddd")
+    // for (var i = 0; i <= 6; i++) {
+    //   const day = moment().add(i, 'days').format("ddd")
+    //   const isCurrent = (day == today);
+    //   this.day.push({ day: day, isCheck: isCurrent });
+    // };
+    
+    this.day.push({ day: 'Sun', isCheck: ('Sun' == today) })
+    this.day.push({ day: 'Mon', isCheck: ('Mon' == today) })
+    this.day.push({ day: 'Tue', isCheck: ('Tue' == today) })
+    this.day.push({ day: 'Wed', isCheck: ('Wed' == today) })
+    this.day.push({ day: 'Thu', isCheck: ('Thu' == today) })
+    this.day.push({ day: 'Fri', isCheck: ('Fri' == today) })
+    this.day.push({ day: 'Sat', isCheck: ('Sat' == today) })
+
+    // for (var i = 0; i <= this.day.length; i++) {
+    //   const element = this.day[i]
+    //   const isCurrent = (element.day == today);
+    //   this.day[i].isCheck=isCurrent;
+    // };
   }
 
   async ngOnInit() {
     this.data = this.navParams.data;
     this.startDate=this.data.startDate;
+    this.recurringData=this.data.recurringData;
+    if(this.recurringData !=undefined){
+      this.isRecurring= this.recurringData.recurring_type
+    }
   }
 
   set() {
-    this.popover.dismiss({ isSub:1});
+    let days = [];
+    let data:any={recurring_type:'',weekly_days:'',repeate_month:'',repeate_year:''};
+    data.recurring_type=this.isRecurring
+    if(this.isRecurring == 'daily'){
+    }else if(this.isRecurring == 'weekly'){
+      days=[]
+      for (let i = 0; i < this.day.length; i++) {
+        const element = this.day[i];
+        if (element.isCheck != undefined && element.isCheck) {
+          days.push(element.day)
+        }
+      }
+      data.weekly_days=days
+      if(days.length ==0){
+        this.globle.showToast('Please at least one day ',1500);
+        return
+      }
+    }else if(this.isRecurring == 'monthly'){
+      data.repeate_month=this.selectedMonth
+      
+    }else if(this.isRecurring == 'yearly'){
+      data.repeate_year=this.selectedYear
+    }
+    
+    this.popover.dismiss({ isSub:1,data:data});
   }
   closeModal() {
     this.popover.dismiss({ isSub:0});
